PixelExperience-Devices/device_oneplus_avicii

avicii: consider lineage_avicii product via the AOSP way
Change-Id: Iab7b6c4d2817af88a133945df8fcc7fb964e35e9
Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

AndroidProducts.mk
@@ -1,0 +1,22 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_avicii.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_avicii-userdebug \
+    lineage_avicii-eng

dkpost3/android_device_oneplus_avicii

AndroidProducts.mk
@@ -1,0 +1,23 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_avicii.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_avicii-eng \
+    lineage_avicii-user \
+    lineage_avicii-userdebug